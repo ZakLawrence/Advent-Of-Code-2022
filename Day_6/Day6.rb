0cd49411fb8d44859ea9a182d978a7320a6cb997
ZakLawrence <zlawrence97@outlook.com>
2022-12-07 21:12:23 +0000

    Update to Day 6 code

--- Day_6/Day6.rb
+++ Day_6/Day6.rb
@@ -3,13 +3,13 @@ def file_content(file_path)
     file_data = file.readlines
 end
 
-def signal_search(file_path)
+def signal_search(file_path,window)
     file_data = file_content(file_path)
     for line in file_data.each do
         chars = line.split('')
         for char,index in chars.each_with_index do
             #puts char, index
-            if chars[index...index+14].uniq.length == 14
+            if chars[index...index+window].uniq.length == window
                 #puts chars[index...index+4]
                 puts index+14
                 break
@@ -19,4 +19,4 @@ def signal_search(file_path)
     end
 end
 
-signal_search("Input.txt")
+signal_search("Input.txt",14)
